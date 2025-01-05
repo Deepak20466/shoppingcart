e711db445345f851589e9137debb6633cbc27124
Deepak20466 <kdeepak162001@gmail.com>
2025-01-06 01:57:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { CartProvider } from './context/CartContext';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Footer from './components/Footer';
+import Cart from './components/Cart';
+
+
+function App() {
+  return (
+    <>
+      <CartProvider>
+        <Header />
+        <Home />
+        <Footer />
+        <Cart />
+      </CartProvider>
+      
+    </>
+  );
+}
+
+export default App;
